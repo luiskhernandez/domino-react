1aee2edeea65c284350524cf9282068625178a20
Luisk Hernandez <luisk.hernandez.macias@gmail.com>
2015-08-24 11:00:31 -0500

    Move card value logic into Card Component

--- components/domino/card.js
+++ components/domino/card.js
@@ -3,8 +3,39 @@ import classNames from 'classnames';
 
 
 let Card = React.createClass({
+  match (param) {
+    let value;
+    switch(param){
+      case 0:
+        value = "blank";
+        break;
+      case 1:
+        value = "one";
+        break
+      case 2:
+        value = "two";
+        break
+      case 3:
+        value = "three";
+        break
+      case 4:
+        value = "four";
+        break
+      case 5:
+        value = "five";
+        break
+      case 6:
+        value = "six";
+        break
+    }
+    return value;
+  },
+  wordValue (array) {
+    return this.match(array[0]) + " " + this.match(array[1]);
+  },
   render () {
-    let classes = classNames('domino', this.props.value);
+    let temp = this.wordValue(this.props.value);
+    let classes = classNames('domino', temp);
     return (
          <div className={classes}></div>
         )
--- components/domino/cards.js
+++ components/domino/cards.js
@@ -6,41 +6,10 @@ import Card from './card';
 let Cards = React.createClass({
   renderList () {
     return this.props.values.map( (value) => {
-      let temp = this.wordValue(value);
       return(
-          <Card value={temp}></Card>
+          <Card value={value} nextCards={this.props.nextCards}></Card>
       )
     });
-  },
-  wordValue (array) {
-    return this.match(array[0]) + " " + this.match(array[1]);
-  },
-  match (param) {
-    let value;
-    switch(param){
-      case 0:
-        value = "blank";
-        break;
-      case 1:
-        value = "one";
-        break
-      case 2:
-        value = "two";
-        break
-      case 3:
-        value = "three";
-        break
-      case 4:
-        value = "four";
-        break
-      case 5:
-        value = "five";
-        break
-      case 6:
-        value = "six";
-        break
-    }
-    return value;
   },
   render () {
     return (
