9b90bdf679fa96b51c34a2723db909faa5bbbe52
Luisk Hernandez <luisk.hernandez.macias@gmail.com>
2015-08-24 11:49:41 -0500

    Create Cards and Shufffle

--- components/pages/play-now.js
+++ components/pages/play-now.js
@@ -2,12 +2,25 @@ import React from 'react';
 import Router from 'react-router';
 import Avatar from '../avatar/avatar';
 import Cards from '../domino/cards'
+import _ from 'underscore';
 
 let Link = Router.Link;
 
 let PlayNow = React.createClass({
+  getInitialState () {
+    let cards = [];
+    _.each(_.range(7), function(item){
+      _.each(_.range(7),function(item2){
+        cards.push([item,item2])
+      })
+    });
+    cards = _.shuffle(cards);
+    return {
+      cards: cards
+    }
+  },
   render () {
-    let values = [[0,0],[0,1],[0,2], [0,3], [0,4], [0,5]];
+    let values = _.sample(this.state.cards,6);
     return (
         <div>
           <h1>Play now</h1>
