47bb063fb701ce1c8a6b8fe72fe414173568451c
Juank Hazbun <juan.hazbun@koombea.com>
2015-08-26 17:59:46 -0500

    Adding facebook and local login

--- server.js
+++ server.js
@@ -1,20 +1,52 @@
-var app = require('express')();
-var express = require('express');
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var express     = require('express');
+var app         = express();
+var port        = 3000;
+var mongoose    = require('mongoose');
+var passport    = require('passport');
+var flash       = require('connect-flash');
+
+var morgan        = require('morgan');
+var cookieParser  = require('cookie-parser');
+var bodyParser    = require('body-parser');
+var session       = require('express-session');
+
+var configDB = require('./config/database.js');
+
+// configuration ===============================================================
+mongoose.connect(configDB.url); // connect to our database
+
+require('./config/passport')(passport); // pass passport for configuration
+
+// set up our express application
+app.use(morgan('dev')); // log every request to the console
+app.use(cookieParser()); // read cookies (needed for auth)
+app.use(bodyParser()); // get information from html forms
+
+app.set('view engine', 'ejs'); // set up ejs for templating
+
+// required for passport
+app.use(session({ secret: 'dominojs' })); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in 
+
+// routes ======================================================================
+require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
+
+// launch ======================================================================
+app.listen(port);
+console.log('The magic happens on port ' + port);
+
+
+// Move Code
+/*var io          = require('socket.io')(http);
+
 var games = require('./routes/games');
 var _ = require('underscore');
-var bodyParser = require('body-parser');
-
-app.set('views', __dirname + '/views')
-app.set('view engine', 'jade')
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-
-app.use(express.static('public'));
 
 var cards = [];
 var users = [];
+
 var createBoardCards = function createBoardCards(){
   _.each(_.range(7), function(item){
     _.each(_.range(item, 7),function(item2){
@@ -25,14 +57,17 @@ var createBoardCards = function createBoardCards(){
   });
 }
 
-app.get('/', function(req, res){
-  res.render('login');
-});
+// Move routes to routes file ================================================
 
 app.post('/users', function(req, res){
   if (_.filter(users, function(user){ return user.email == req.body.email }).length == 0) {
     console.log('Create user');
-    users.push({email: req.body.email});
+    if (users.length == 0) {
+      selected = true;
+    } else {
+      selected = false;
+    }
+    users.push({email: req.body.email, selected: selected});
     res.redirect('/play');
   } else {
     console.log('User already in the list');
@@ -47,7 +82,6 @@ app.get('/play', function(req, res){
   res.render('index');
 });
 
-// app.use('/games', games);
 app.get('/games/deal/card', function(req, res, next) {
   cards = _.shuffle(cards);
   var newcards = cards.splice(7);
@@ -56,13 +90,10 @@ app.get('/games/deal/card', function(req, res, next) {
   res.json({cards: playercards});
 });
 
+// End Routes ===============================================================
+
 io.on('connection', function(socket){
   socket.on('fetchUsers', function(){
     io.emit('fetchUsers', {users: users});
   });
-});
-
-
-http.listen(3000, function(){
-  console.log('listening on *:3000');
 });
