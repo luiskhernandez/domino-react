53625d98090508e1b0558c3f77841fcf449e1a98
Luisk Hernandez <luisk.hernandez.macias@gmail.com>
2015-08-31 13:16:36 -0500

    Redirect to game after signup

--- app/routes.js
+++ app/routes.js
@@ -40,7 +40,7 @@ module.exports = function(app, passport, board) {
 
     // process the signup form
     app.post('/signup', passport.authenticate('local-signup', {
-        successRedirect : '/profile', // redirect to the secure profile section
+        successRedirect : '/play', // redirect to the secure profile section
         failureRedirect : '/signup', // redirect back to the signup page if there is an error
         failureFlash : true // allow flash messages
     }));
